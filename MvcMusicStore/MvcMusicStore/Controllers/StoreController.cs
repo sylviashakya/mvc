7a66eaff4c6608f7500f7d3c6f652af2d6987cbd
sylvia <shakyasylvia@gmail.com>
2015-05-27 17:11:23 +0545

    mvc initiate

---
+++ MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+
+        public ActionResult Index()
+        {
+            var genres = new List<Genre>
+            {
+                new Genre {Name="hiphop"},
+                new Genre{Name= "pop"},
+                new Genre{Name="rap"}
+            };
+            return View(genres);
+        }
+        
+        public ActionResult Browse(string genre)
+        {
+            var genreModel = new Genre { Name = genre };
+            return View(genreModel);
+        }
+        public ActionResult Details(int id)
+        {
+            var album = new Album { Title = "Album" + id };
+            return View(album);
+        }
+
+
+    }
+}
